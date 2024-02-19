c9a3bf28b8ce5f3603562bf4b804201c5b9f6bc6
fbfreitas <ffreitas@cc.isel.ipl.pt>
2024-02-19 10:26:19 +0000

    Start commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 2324-2-common
+Software Laboratory, 2023/2024, Spring semester
---
+++ src/test/kotlin/pt/isel/ls/ExampleTest.kt
@@ -0,0 +1,22 @@
+package pt.isel.ls
+
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ExampleTest {
+    @Test
+    fun example() {
+        // arrange | given
+        val a = 1
+        val b = 2
+
+        // act | when
+        val result = a + b
+
+        // assert | then
+        assertEquals(3, result)
+    }
+    
+}
